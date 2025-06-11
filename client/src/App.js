9b9dd2fd8113d4e3a4715c0ee455b09aa9154ec9
Rashmi <rashmi8shahi@gmail.com>
2025-06-12 01:49:23 +0530

    Added basic Navbar with routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
-
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
